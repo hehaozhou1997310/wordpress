534245d6eecf53402c55964791b1baccd1616298
A-Dhaidan <88009409+A-Dhaidan@users.noreply.github.com>
2021-09-12 23:29:07 +1000

    Updates made

--- wp-content/plugins/AliDhaidanpackages-plugin/AliDhaidanpackages-plugin.php
+++ wp-content/plugins/AliDhaidanpackages-plugin/AliDhaidanpackages-plugin.php
@@ -32,6 +32,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 Copyright 2005-2015 Automattic, Inc.
 */
 
+add_filter( 'show_admin_bar','__return_false');
+
 defined('ABSPATH') or die( 'You do not have authorised access to this file!');
 
 class AliDhaidanPackages
@@ -43,14 +45,45 @@ class AliDhaidanPackages
     // can be access only within the class itself
 
     // Private
-    
+    // can be accessed by only within the class itself
+
+    public $plugin;
 
     function __construct() {
-      add_action( 'init', array( $this,'custom_post_type') );
+      $this->plugin = plugin_basename(__FILE__);
     }
 
-    function register( ) {
+    public function register( ) {
       add_action('admin_enqueue_scripts', array( $this, 'enqueue'));
+
+      add_action('admin_menu', array( $this, 'add_admin_pages'));
+
+      
+
+      add_filter( "plugin_action_links_$this->plugin", array($this, 
+      'settings_link'));
+    }
+
+    public function settings_link( $links) {
+      $settings_link = '<a href="admin.php?page=Weekly_packages">Settings
+      </a>';
+      array_push($links, $settings_link);
+      return $links;
+    }
+
+
+    public function add_admin_pages() {
+      add_menu_page('Weekly Packages', 'Packages', 'manage_options', 
+      'Weekly_packages', array( $this, 'admin_index'), 'dashicons-admin-media', 110 );
+
+    }
+
+    public function admin_index() {
+      require_once plugin_dir_path(__FILE__) . 'templates/Admin.php' ;
+    }
+
+    protected function create_post_type() {
+      add_action( 'init', array( $this,'custom_post_type') );
     }
 
     function activate() {
@@ -66,6 +99,9 @@ class AliDhaidanPackages
     }
 
 
+
+
+
     function custom_post_type() {
       register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
     }
@@ -76,15 +112,29 @@ class AliDhaidanPackages
       wp_enqueue_style('mypluginstyle', plugins_url('/assets/myscript.js ',__FILE__));
 
     }
+    
+    
 }
+
+
+
 
 if (class_exists( 'AliDhaidanPackages')) {
   $alidhaidanPackages = new AliDhaidanPackages();
   $alidhaidanPackages->register();
+  
+  
 }
+
+
+
+
 
 // activation
 register_activation_hook( __FILE__, array( $alidhaidanPackages, 'activate' ) );
 
 // deactivation
 register_deactivation_hook( __FILE__, array( $alidhaidanPackages, 'deactivate' ) );
+
+
+
---
+++ wp-content/plugins/AliDhaidanpackages-plugin/Templates/Admin.php
@@ -0,0 +1,24 @@
+<h1> Weekly Packages </h1>
+
+<body>
+<form action="clinicdetails.php" method="post">
+		<label for="clinic">Please confirm selected Medical Centre: </label>
+			<select id="clinic" name="clinic" size="1">
+				<option value="health first">Health First</option>
+				<option value="health point">Health Point(North Shore)</option>
+				<option value="health point">Health Point(Parramatta)</option>
+				<option value="green clinic">Green Clinic(Eastwood)</option>
+				<option value="green clinic">Green Clinic(Chatswood)</option>
+				<option value="bright health care">Bright Health Care</option>
+				<option value="quick recovery">Quick Recovery</option>
+				<option value="lightweight nutrition">Lightweight Nutrition</option>
+				<option value="fast recovery">Fast Recovery</option>
+				<option value="pain ease">Pain Ease(Camplbelltown)</option>
+				<option value="pain ease">Pain Ease(Parramatta)</option>
+				<option value="relief clinic">Relief Clinic</option>
+				<option value="shining smile">Shining Smile</option>
+				<option value="wonder ward">Wonder Ward</option>
+				<option value="medireco">Medireco</option>
+			</select><br>
+
+</body>
